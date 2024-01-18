bde6824da072c71fd84c7ac531fbed9a3e9b3d8f
sajeban <sajerex@gmail.com>
2024-01-18 17:39:59 +0800

    project init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ECSStateNotifier
+# Careverse Helper Lambdas
+
+
+### Install Serverless
+
+```
+$ npm install -g serverless
+```
+
+
+### Deployment
+
+In order to deploy the project, you need to run the following command:
+
+```
+$ serverless deploy --stage dev
+```
---
+++ src/notifyAlerts.js
@@ -0,0 +1,29 @@
+const aws = require('aws-sdk');
+// Change the region as needed
+const ses = new aws.SES({region: 'ap-southeast-1'});
+
+exports.notifyAlerts = async (event) => {
+    const params = {
+        // Verify this email in SES
+        Source: `critical-alerts@email-address.com`,
+
+        // Verify recipient email as well
+        Destination: {
+            ToAddresses: ['email-address'],
+        },
+        Message: {
+            Subject: {Data: `Server Stopped at ${event.time}`},
+            Body: {
+                Text: {Data: `More details: ${JSON.stringify(event)}`}
+            }
+        }
+    };
+
+    try {
+        await ses.sendEmail(params).promise();
+        console.log('Email sent successfully');
+    } catch (error) {
+        console.error('Error sending email', error);
+        throw error;
+    }
+};
